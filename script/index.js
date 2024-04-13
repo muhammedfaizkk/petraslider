aa7483b3be4aace44191ff93ce3222a000903392
Muhammed faiz kk <fayizkk5@gmail.com>
2024-04-13 17:49:06 +0530

    initialcommit

---
+++ script/index.js
@@ -0,0 +1,31 @@
+// Initialize Swiper for '.swiper' class
+var mainSwiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+
+// Initialize Swiper for '.mySwiper' class
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// GSAP ScrollTrigger animation for elements with class '.items'
+
